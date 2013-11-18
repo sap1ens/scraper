ed29b944bcde89a71ff43a49c9250a5fb0648c14
sap1ens <sapiensy@gmail.com>
2013-11-17 16:55:19 -0800

    Update profiles config, keep only Canada for now

--- src/main/scala/com/sap1ens/Scraper.scala
+++ src/main/scala/com/sap1ens/Scraper.scala
@@ -1,32 +1,28 @@
 package com.sap1ens
 
 import akka.actor.{Props, ActorSystem}
-import com.typesafe.config.ConfigFactory
-import scala.collection.convert.WrapAsScala._
-import scala.concurrent.ExecutionContext
-import ExecutionContext.Implicits.global
-import akka.pattern.ask
-import akka.util.Timeout
-import scala.concurrent.duration._
-import scala.util.{Success, Failure}
+import com.typesafe.config.{ConfigObject, ConfigFactory}
 import com.sap1ens.CollectorService._
-import com.sap1ens.PageParser._
+import collection.JavaConversions._
+import collection.JavaConverters._
 
 object Scraper extends App {
 
     val config = ConfigFactory.load()
     val system = ActorSystem("craigslist-scraper-system")
 
-    val profiles = Profile(
-        "US", config.getString("profile.US.pattern"), config.getStringList("profile.US.cities").toList
-    ) :: Profile(
-        "CA", config.getString("profile.CA.pattern"), config.getStringList("profile.CA.cities").toList
-    ) :: Nil
+    val profiles = for {
+        profile: ConfigObject <- config.getObjectList("profiles").asScala
+    } yield Profile(
+        profile.get("country").unwrapped().toString,
+        profile.get("pattern").unwrapped().toString,
+        profile.get("cities").unwrapped().asInstanceOf[java.util.ArrayList[String]].toList
+    )
 
     val searchString = config.getString("search")
     val resultsFolder = config.getString("results.folder")
     val resultsMode = config.getString("results.mode")
 
-    val collectorService = system.actorOf(Props(new CollectorService(profiles, searchString, resultsFolder, resultsMode)), "CollectorService")
+    val collectorService = system.actorOf(Props(new CollectorService(profiles.toList, searchString, resultsFolder, resultsMode)), "CollectorService")
     collectorService ! StartScraper
 }
